fe40f0b0f045e7435f7eb819a97788d021b5be77
Hamid Hassani <hamidhassani119@gmail.com>
2022-08-30 13:12:29 +0430

    send notification feature added to the app

--- server/index.js
+++ server/index.js
@@ -8,15 +8,45 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/auth.js");
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require("twilio")(accountSid, authToken);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.use("/auth", authRoutes);
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user.id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message sent!"))
+            .catch((error) => console.log(error));
+        }
+      });
+
+    return res.status(200).send("Message sent!");
+  }
+
+  return res.status(200).send("Not a new message request!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
